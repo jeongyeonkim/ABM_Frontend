6b73ccb56c68f19c069de537d2d46f1aeca47650
jeongyeon <yeon8480@gmail.com>
2019-09-25 16:13:38 +0900

    axois

--- src/main.js
+++ src/main.js
@@ -3,12 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import axios from 'axios'
+import VueAxois from 'vue-axios'
+
 import 'vuetify/dist/vuetify.min.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import VueRouter from 'vue-router';
 
 Vue.use(vuetify)
 Vue.use(VueRouter)
+Vue.use(axios, VueAxois)
 Vue.config.productionTip = false
 
 new Vue({
