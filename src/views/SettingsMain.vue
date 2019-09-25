753e6e807d92c6cc77e5a17c1fe7cf46bff26df8
yuzin97 <97dbwls1224@gmail.com>
2019-09-25 13:18:07 +0900

    settingsmain is not finised!

--- src/views/SettingsMain.vue
+++ src/views/SettingsMain.vue
@@ -6,7 +6,24 @@
         <v-layout align-center justify-center>
           <v-flex xs12 sm8 md4>
           <Toolbar></Toolbar>
-          <v-icon>settings_applications</v-icon>Settings
+          <v-row>
+            <v-flex>
+            <p class="text-center font-weight-medium ma-5">Settings<v-icon color="#E65100">settings</v-icon></p>
+            </v-flex>
+          </v-row>
+          <v-row>
+            <v-btn flat block class="text-xs-center mb-5" @click="goAccount()" >My Account</v-btn>
+            <v-btn flat block class="text-xs-center mb-5" >Log out</v-btn>
+            <v-btn flat block class="text-xs-center mb-5" @click="dialog=true" >Register Store</v-btn>
+
+            <v-dialog v-model="dialog" persistent max-width="500px">
+            <v-card>
+              <v-card-tiitle>
+                <span class="headline">Register Store</span>
+              </v-card-tiitle>
+            </v-card>
+            </v-dialog>
+          </v-row>
           </v-flex>
         </v-layout>
       </v-container>
@@ -20,14 +37,19 @@
 import Toolbar from '../components/Toolbar.vue';
 
 export default {
-  name: 'default',
   data () {
     return {
+      dialog : false,
     }
   },
   components: {
     Toolbar,
   },
-  methods: {}
+  methods: {
+    goAccount(){
+      this.$router.push({name: 'accountsetting'})
+      }
+      
+  }
 }
 </script>
